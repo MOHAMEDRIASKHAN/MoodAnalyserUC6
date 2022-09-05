db31ce8fc5eb39468cf8ec2eb855dc689555c074
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 08:23:35 -0700

    MoodAnalyserException.cs

---
+++ MoodAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC6
+{
+    public class MoodAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            ENTERD_NULL, ENTERED_EMPTY,
+            NO_SUCH_CLASS, NO_SUCH_METHOD
+        }
+
+        public ExceptionType type;
+        public MoodAnalyserException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
